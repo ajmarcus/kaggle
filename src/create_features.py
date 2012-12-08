bd60174669bd084c88e2d057c15cd79a6a48a89a
Ubuntu <ubuntu@ip-10-148-162-172.ec2.internal>
2012-12-08 23:17:59 +0000

    adding features with nltk

---
+++ src/create_features.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import nltk, re, fileinput
+
+punkt = nltk.data.load('tokenizers/punkt/english.pickle')
+word_tokenizer = nltk.tokenize.WordPunctTokenizer()
+stopwords = set(nltk.corpus.stopwords.words('english'))
+real_word = re.compile('[a-z]+')
+
+header = ['num_sentences','num_unique_words'] 
+result = ['NA','NA']
+
+print "\t".join(header)
+
+for line in fileinput.input():
+  sentences = punkt.tokenize(line)
+  # Number of sentences
+  result[0] = len(sentences)
+
+  unique_words = [ word for word in set(word_tokenizer.tokenize(line)) if word not in stopwords and real_word.match(word) ]
+  # Number of unique words
+  result[1] = len(unique_words)
+
+  print "\t".join(result)
+
