bec0fe22596868fd4128eafd2dc72dad5eb08d12
Ubuntu <ubuntu@ip-10-148-162-172.ec2.internal>
2012-12-08 23:43:55 +0000

    fixed bug added tests and run script

--- src/create_features.py
+++ src/create_features.py
@@ -5,10 +5,10 @@ import nltk, re, fileinput
 punkt = nltk.data.load('tokenizers/punkt/english.pickle')
 word_tokenizer = nltk.tokenize.WordPunctTokenizer()
 stopwords = set(nltk.corpus.stopwords.words('english'))
-real_word = re.compile('[a-z]+')
+real_word = re.compile('[A-Za-z][a-z]+')
 
-header = ['num_sentences','num_unique_words'] 
-result = ['NA','NA']
+header = ['num_sentences','num_unique_words','num_words'] 
+result = ['NA','NA','NA']
 
 def uni(input):
   r = ''
@@ -25,9 +25,13 @@ for line in fileinput.input():
   # Number of sentences
   result[0] = len(sentences)
 
-  unique_words = [ word for word in set(word_tokenizer.tokenize(line)) if word not in stopwords and real_word.match(word) ]
+  words = [ word for word in word_tokenizer.tokenize(line) if word not in stopwords and real_word.match(word) ]
+  unique_words = set(words)
   # Number of unique words
   result[1] = len(unique_words)
+
+  # Number of total words
+  result[2] = len(words)
 
   print "\t".join(map(uni, result))
 
