77c1cb948be1815273ae8837a4b7defe7dc79f70
Ubuntu <ubuntu@ip-10-140-128-27.ec2.internal>
2012-12-09 06:04:43 +0000

    added container for scikit learn

---
+++ data/container/split_data.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import fileinput
+
+i = 0
+file_pattern = "./%(grade)i/%(num)i.txt"
+
+def uni(input):
+  r = ''
+  try:
+    r = unicode(input).encode('ascii', 'ignore')
+  except Exception, e:   
+    raise e
+  return r
+
+for line in fileinput.input():
+  i = i + 1
+  essay,grade = line.split("\t")
+  f = file(file_pattern % { 'grade': int(grade), 'num': i }, 'w')
+  f.write(essay)
