cfd8024f4e9ad70e3be8d3af17e42a474d3fa36b
ajmarcus <little@merman>
2012-12-10 21:58:44 -0500

    grid search

--- src/grid_search.py
+++ src/grid_search.py
@@ -11,12 +11,13 @@ from pprint import pprint
 from time import time
 import logging
 
-from sklearn.datasets import fetch_20newsgroups
+from sklearn.datasets import load_files
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.linear_model import SGDClassifier
+from sklearn.decomposition import SparsePCA
+from sklearn.linear_model.sparse import SGDClassifier
 from sklearn.grid_search import GridSearchCV
 from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfTransformer
 
 # Display progress logs on stdout
 logging.basicConfig(level=logging.INFO,
@@ -25,17 +26,36 @@ logging.basicConfig(level=logging.INFO,
 
 ###############################################################################
 # Load some categories from the training set
-categories = [
-    'alt.atheism',
-    'talk.religion.misc',
-]
+categories = ['1','2','3']
+
 # Uncomment the following to do the analysis on all the categories
 #categories = None
 
-print "Loading 20 newsgroups dataset for categories:"
+print "Loading essay data:"
 print categories
 
-data = fetch_20newsgroups(subset='train', categories=categories)
+data = load_files('../data/container', categories=categories, shuffle=True)
+
+#train_size = int(len(data.data)*0.8)
+#test_size = train_size + 1
+
+#data_train = Bunch(data=data.data[0:train_size],
+#                   filenames=data.filenames[0:train_size],
+#                   target_names=data.target_names,
+#                   target=data.target[0:train_size],
+#                   DESCR=data.DESCR)
+
+#data_test = Bunch(data=data.data[test_size:len(data.data)],
+#                   filenames=data.filenames[test_size:len(data.data)],
+#                   target_names=data.target_names,
+#                   target=data.target[test_size:len(data.data)],
+#                   DESCR=data.DESCR)
+
+print 'data loaded'
+
+categories = data.target_names    # for case categories == None
+
+
 print "%d documents" % len(data.filenames)
 print "%d categories" % len(data.target_names)
 print
@@ -45,21 +65,23 @@ print
 # classifier
 pipeline = Pipeline([
     ('vect', CountVectorizer()),
-    ('tfidf', TfidfTransformer()),
-    ('clf', SGDClassifier()),
+('tfidf', TfidfTransformer()),
+#    ('pca', SparsePCA()),
+    ('clf', SGDClassifier())
 ])
 
 parameters = {
 # uncommenting more parameters will give better exploring power but will
 # increase processing time in a combinatorial way
+    'vect__analyzer': ('word', 'char_wb'),
     'vect__max_df': (0.5, 0.75, 1.0),
-#    'vect__max_features': (None, 5000, 10000, 50000),
+    'vect__max_features': (None, 5000, 10000, 50000),
     'vect__max_n': (1, 2),  # words or bigrams
-#    'tfidf__use_idf': (True, False),
-#    'tfidf__norm': ('l1', 'l2'),
+    'tfidf__use_idf': (True, False),
+    'tfidf__norm': ('l1', 'l2'),
     'clf__alpha': (0.00001, 0.000001),
     'clf__penalty': ('l2', 'elasticnet'),
-#    'clf__n_iter': (10, 50, 80),
+    'clf__n_iter': (10, 50, 80)
 }
 
 if __name__ == "__main__":
@@ -68,7 +90,7 @@ if __name__ == "__main__":
 
     # find the best parameters for both the feature extraction and the
     # classifier
-    grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1, verbose=1)
+    grid_search = GridSearchCV(pipeline, parameters, n_jobs=4, verbose=1)
 
     print "Performing grid search..."
     print "pipeline:", [name for name, _ in pipeline.steps]
